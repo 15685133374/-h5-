7cce04e64c63dab23e38008311a329cf6f67c3a2
zhaoyang <zhaoyang_an@sina.com>
2019-12-11 14:29:41 +0800

    delete modules

--- front-end/h5/node_modules_back/vue2-medium-editor/index.js
+++
@@ -1,5 +0,0 @@
-// Because Medium Editor does not support SSR, we just render in that case
-import interactive from './interactive'
-import renderOnly from './renderOnly'
-
-export default process.browser ? interactive : renderOnly
